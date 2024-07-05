38e410ec5d0f63b709c83adab8845a06fbcbe1f7
Ethan Zhang <ethanz4@illinois.edu>
2024-07-05 14:06:52 -0700

    up and running with dream

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let string_of_elt elt = Format.asprintf "%a" (Tyxml.Html.pp_elt ()) elt
+
+let root _request =
+  Dream.html
+  @@ string_of_elt
+  @@
+  let open Tyxml.Html in
+  html (head (title (txt "Todos")) []) (body [ h1 [ txt "Todos" ] ])
+;;
+
+let () = Dream.run @@ Dream.logger @@ Dream.router [ Dream.get "/" root ]
