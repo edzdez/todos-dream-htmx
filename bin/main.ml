f45869d92b2e3c862d940a1a8d2005b339bc5509
Ethan Zhang <ethanz4@illinois.edu>
2024-07-05 18:05:18 -0700

    add persistence

--- bin/main.ml
+++ bin/main.ml
@@ -22,50 +22,71 @@ module TodoItem = struct
     }
   [@@deriving yojson]
 
-  let store : ts ref =
-    ref
-      [ { id = 0; title = "Hello"; description = "World" }
-      ; { id = 1; title = "Test"; description = "Todo" }
-      ]
-  ;;
+  module Persistence = struct
+    module T = Caqti_type
+    open Caqti_request.Infix
 
-  let store_lock = Mutex.create ()
-  let get_all () = Mutex.protect store_lock (fun _ -> !store)
+    let todo =
+      let encode { id; title; description } = Ok (id, title, description) in
+      let decode (id, title, description) = Ok { id; title; description } in
+      let rep = T.tup3 T.int T.string T.string in
+      T.custom ~encode ~decode rep
+    ;;
 
-  let get id =
-    Mutex.protect store_lock (fun _ -> List.find_opt (fun todo -> todo.id = id) !store)
-  ;;
-
-  let create (request : create_request) =
-    Mutex.protect store_lock (fun _ ->
-      let id = List.length !store in
-      let todo = { id; title = request.title; description = request.description } in
-      store := todo :: !store;
-      todo)
-  ;;
-
-  let update (id : int) (request : update_request) =
-    Mutex.protect store_lock (fun _ ->
-      let todo = List.find (fun todo -> todo.id = id) !store in
-      let todo =
-        { id
-        ; title = (if request.title = "" then todo.title else request.title)
-        ; description =
-            (if request.description = "" then todo.description else request.description)
-        }
+    let create =
+      let query =
+        (T.tup2 T.string T.string ->! todo)
+        @@ "INSERT INTO todos(title, description) VALUES (?, ?) RETURNING *"
       in
-      store := todo :: List.filter (fun todo -> todo.id <> id) !store;
-      todo)
-  ;;
+      fun ({ title; description } : create_request) (module Db : Caqti_lwt.CONNECTION) ->
+        let%lwt todo = Db.find query (title, description) in
+        Caqti_lwt.or_fail todo
+    ;;
 
-  let remove id =
-    Mutex.protect store_lock (fun _ ->
-      store := List.filter (fun todo -> todo.id <> id) !store)
+    let get_all =
+      let query = (T.unit ->* todo) @@ "SELECT * FROM todos" in
+      fun (module Db : Caqti_lwt.CONNECTION) ->
+        let%lwt todos = Db.collect_list query () in
+        Caqti_lwt.or_fail todos
+    ;;
+
+    let get =
+      let query = (T.int ->? todo) @@ "SELECT * FROM todos WHERE id = ?" in
+      fun id (module Db : Caqti_lwt.CONNECTION) ->
+        let%lwt todo = Db.find_opt query id in
+        Caqti_lwt.or_fail todo
+    ;;
+
+    let update =
+      let query =
+        (T.tup3 T.string T.string T.int ->. T.unit)
+        @@ "UPDATE todos SET title = ?, description = ? WHERE id = ?"
+      in
+      fun { id; title; description } (module Db : Caqti_lwt.CONNECTION) ->
+        let%lwt unit = Db.exec query (title, description, id) in
+        Caqti_lwt.or_fail unit
+    ;;
+
+    let remove =
+      let query = (T.int ->. T.unit) @@ "DELETE FROM todos WHERE id = ?" in
+      fun id (module Db : Caqti_lwt.CONNECTION) ->
+        let%lwt unit = Db.exec query id in
+        Caqti_lwt.or_fail unit
+    ;;
+  end
+
+  let update (todo : t) (request : update_request) =
+    { id = todo.id
+    ; title = (if request.title = "" then todo.title else request.title)
+    ; description =
+        (if request.description = "" then todo.description else request.description)
+    }
   ;;
 end
 
-let get_all_todos _request =
-  Dream.json @@ Yojson.Safe.to_string @@ TodoItem.ts_to_yojson @@ TodoItem.get_all ()
+let get_all_todos request =
+  let%lwt todos = Dream.sql request TodoItem.Persistence.get_all in
+  Dream.json @@ Yojson.Safe.to_string @@ TodoItem.ts_to_yojson todos
 ;;
 
 let get_todo request =
@@ -73,7 +94,8 @@ let get_todo request =
   match int_of_string_opt id with
   | None -> Dream.empty `Bad_Request
   | Some id ->
-    (match TodoItem.get id with
+    let%lwt maybe_todo = Dream.sql request (TodoItem.Persistence.get id) in
+    (match maybe_todo with
      | None -> Dream.empty `Not_Found
      | Some todo -> Dream.json @@ Yojson.Safe.to_string @@ TodoItem.to_yojson todo)
 ;;
@@ -87,8 +109,8 @@ let create_todo request =
        Dream.error (fun log ->
          log ~request "Failed to parse TodoItem.create_request with error: %s" e);
        Dream.empty `Bad_Request
-     | Ok request ->
-       let todo = TodoItem.create request in
+     | Ok create_request ->
+       let%lwt todo = Dream.sql request (TodoItem.Persistence.create create_request) in
        Dream.json
          ~status:`Created
          ~headers:[ "Location", Printf.sprintf "/api/todos/%d" todo.id ]
@@ -102,9 +124,10 @@ let update_todo request =
   match int_of_string_opt id with
   | None -> Dream.empty `Bad_Request
   | Some id ->
-    (match TodoItem.get id with
+    let%lwt todo = Dream.sql request (TodoItem.Persistence.get id) in
+    (match todo with
      | None -> Dream.empty `Not_Found
-     | Some _ ->
+     | Some todo ->
        (match Dream.header request "Content-Type" with
         | Some "application/json" ->
           let%lwt body = Dream.body request in
@@ -113,8 +136,9 @@ let update_todo request =
              Dream.error (fun log ->
                log ~request "Failed to parse TodoItem.update_request with error: %s" e);
              Dream.empty `Bad_Request
-           | Ok request ->
-             let todo = TodoItem.update id request in
+           | Ok update_request ->
+             let todo = TodoItem.update todo update_request in
+             let%lwt () = Dream.sql request (TodoItem.Persistence.update todo) in
              Dream.json
                ~status:`OK
                ~headers:[ "Content-Location", Printf.sprintf "/api/todos/%d" id ]
@@ -128,10 +152,11 @@ let delete_todo request =
   match int_of_string_opt id with
   | None -> Dream.empty `Bad_Request
   | Some id ->
-    (match TodoItem.get id with
+    let%lwt todo = Dream.sql request (TodoItem.Persistence.get id) in
+    (match todo with
      | None -> Dream.empty `Not_Found
      | Some _ ->
-       TodoItem.remove id;
+       let%lwt () = Dream.sql request (TodoItem.Persistence.remove id) in
        Dream.empty `No_Content)
 ;;
 
@@ -146,6 +171,8 @@ let root _request =
 let () =
   Dream.run
   @@ Dream.logger
+  @@ Dream.sql_pool "sqlite3:sqlite.db"
+  @@ Dream.memory_sessions
   @@ Dream.router
        [ Dream.get "/" root
        ; Dream.scope
